8810c6c74c033812f7bfc8aa3279e4b556df1fb2
milenegj <milene.jannetti@gmail.com>
2023-06-12 11:59:11 -0600

    created app initial configuration

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import router from "./routers";
+import { errorHandler } from "./middlewares/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+app.use(errorHandler);
+
+export default app;
